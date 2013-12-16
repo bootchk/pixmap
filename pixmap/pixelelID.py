cd7c32cf0a2bf6949436b94cd323bef5d286fcd3
Lloyd Konneker <bootch@nc.rr.com>
2013-12-16 06:55:32 -0500

    Equality operator

--- pixmap/pixelelID.py
+++ pixmap/pixelelID.py
@@ -4,9 +4,14 @@
 class PixelelID(object):
   '''
   PixelelID is a 3D coord for a pixelel,
-  i.e. 2D coord of pixel within pixmap and 1D index of pixelel within pixel
+  i.e. 2D coord of pixel within pixmap and 1D index of pixelel within pixel.
+  
+  A named tuple
   '''
   
   def __init__(self, coord, pixelelIndex):
     self.coord = coord
     self.pixelelIndex = pixelelIndex
+
+  def __eq__(self, other):
+    return self.coord == other.coord and self.pixelelIndex == other.pixelelIndex
