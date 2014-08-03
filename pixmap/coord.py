2623e71086baee7c9289fb5fcd3be9ef92905fb4
bootchk <bootch@nc.rr.com>
2014-08-02 20:00:14 -0400

    Coord scalar multiplication

--- pixmap/coord.py
+++ pixmap/coord.py
@@ -24,17 +24,37 @@ class Coord(object):
   def __eq__(self, other):
     return self.x == other.x and self.y == other.y
   
+  '''
+  Vector-like operations
+  '''
   def __add__(self, other):
     return Coord( self.x + other.x, self.y + other.y )
     
   def __sub__(self, other):
     return Coord( self.x - other.x, self.y - other.y )
   
+  
+  '''
+  Scalar operations.
+  '''
   def addScalar(self, scalar):
     assert isinstance(scalar, int)
     # Safer would be: return Coord( int(self.x + scalar), int(self.y + scalar) )
     return Coord( self.x + scalar, self.y + scalar )
     
+  def __mul__(self, scalar):
+    '''
+    scalar isInstance(Number) i.e. can be a float.
+    If scalar isInstance(Float), return floor of result.
+    
+    !!! Might not be suitable to scale pixmap coordinates.
+    Especially for coords that define a rect.
+    Study that pixels are rects, and coords in pixmaps may be +/- 1.
+    '''
+    return Coord( int(self.x * scalar), int(self.y * scalar) )
+  
+  
+  
   def toTuple(self):
     return (self.x, self.y)
   
